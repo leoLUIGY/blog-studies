a5c4e6ae12e93b7ddaea8f110c05beeee2bba666
leoLUIGY <luigyleonidas@gmail.com>
2025-06-24 11:52:18 -0300

    primeiro commit

---
+++ data/posts.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+$posts = [
+    [
+        'id' => 1,
+        'title' => 'Programando em PHP' ,
+        'description' => 'Aprenda a programação orintada a objetos com php e suas nuanses.',
+        'tags' => ['programação', 'PHP', 'OOP', 'logica'],
+        'img' => 'prog-1.jpg'
+    ],
+    [
+        'id' => 2,
+        'title' => 'Programando em PHP' ,
+        'description' => 'Aprenda a programação orintada a objetos com php e suas nuanses.',
+        'tags' => ['programação', 'PHP', 'OOP', 'logica'],
+        'img' => 'prog-1.jpg'
+    ],
+       [
+        'id' => 3,
+        'title' => 'Programando em PHP' ,
+        'description' => 'Aprenda a programação orintada a objetos com php e suas nuanses.',
+        'tags' => ['programação', 'PHP', 'OOP', 'logica'],
+        'img' => 'prog-1.jpg'
+    ],
+         [
+        'id' => 4,
+        'title' => 'Programando em PHP' ,
+        'description' => 'Aprenda a programação orintada a objetos com php e suas nuanses.',
+        'tags' => ['programação', 'PHP', 'OOP', 'logica'],
+        'img' => 'prog-1.jpg'
+    ],
+];
